58522c7e6b9a5b81ac5dc85a12a9acb8d681adb9
melanibe <melanibe@student.ethz.ch>
2018-12-19 17:59:33 +0100

    predict_main created

---
+++ code/mains/predict_main.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+from data_loader.data_generator import DataGenerator, DataTestLoader
+from models.DeepYeast_model import DeepYeastModel
+from utils.config import process_config
+from utils.dirs import create_dirs
+from utils.logger import Logger
+from utils.utils import get_args
+from utils.predictor import Predictor
+
+def main():
+    """ Loads the model from the checkpoint dir
+    as specified in the given config file.
+    Calls the prediction function to save the 
+    prediction csv file to the checkpoint dir.
+    """
+    # capture the config path from the run arguments
+    # then process the json configuration file
+    try:
+        args = get_args()
+        config = process_config(args.config)
+
+    except Exception:
+        print("missing or invalid arguments")
+        exit(0)
+
+    # create tensorflow session
+    sess = tf.Session()
+    # create your data generator
+    testIterator = DataTestLoader(config)
+    # create an instance of the model you want
+    model = DeepYeastModel(config)
+    # load model if exists
+    model.load(sess)
+    # here you predict from your model
+    predictor = Predictor(sess, model, config)
+    predictor.predict(testIterator)
+
+
+if __name__ == '__main__':
+    main()
