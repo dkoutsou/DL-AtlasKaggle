735ad76b9fac8cf17526a7525901d76ce6a7b98f
melanibe <melanibe@student.ethz.ch>
2019-01-05 15:55:27 +0100

    new model def

--- code/models/CBDP4_model.py
+++ code/models/CBDP4_model.py
@@ -11,8 +11,8 @@ class CBDP4Model(BaseModel):
     def build_model(self):
         super(CBDP4Model, self).init_build_model()
         print(self.input_layer.get_shape())
-        # Block 1
-        x = tf.layers.conv2d(self.input_layer, 64, 3,
+        # Block 1 - 64
+        x = tf.layers.conv2d(self.input_layer, 8, 3,
                              padding='same', name='conv1')
         x = tf.layers.batch_normalization(
             x, training=self.is_training, name='bn1')
@@ -21,8 +21,8 @@ class CBDP4Model(BaseModel):
         x = tf.layers.max_pooling2d(
             x, pool_size=(2, 2), strides=(2, 2), name='pool1')
         print(x.get_shape())
-        # Block 2
-        x = tf.layers.conv2d(x, 128, 3, padding='same', name='conv2')
+        # Block 2 - 128
+        x = tf.layers.conv2d(x, 16, 3, padding='same', name='conv2')
         x = tf.layers.batch_normalization(
             x, training=self.is_training, name='bn2')
         x = tf.nn.relu(x, name='act2')
@@ -30,8 +30,8 @@ class CBDP4Model(BaseModel):
         x = tf.layers.max_pooling2d(
             x, pool_size=(2, 2), strides=(2, 2), name='pool2')
         print(x.get_shape())
-        # Block 3
-        x = tf.layers.conv2d(x, 256, 3, padding='same', name='conv3')
+        # Block 3 - 256
+        x = tf.layers.conv2d(x, 32, 3, padding='same', name='conv3')
         x = tf.layers.batch_normalization(
             x, training=self.is_training, name='bn3')
         x = tf.nn.relu(x, name='act3')
@@ -39,8 +39,8 @@ class CBDP4Model(BaseModel):
         x = tf.layers.max_pooling2d(
             x, pool_size=(2, 2), strides=(2, 2), name='pool3')
         print(x.get_shape())
-        # Block 4
-        x = tf.layers.conv2d(x, 256, 3, padding='same', name='conv4')
+        # Block 4 - 256
+        x = tf.layers.conv2d(x, 64, 3, padding='same', name='conv4')
         x = tf.layers.batch_normalization(
             x, training=self.is_training, name='bn4')
         x = tf.nn.relu(x, name='act4')
