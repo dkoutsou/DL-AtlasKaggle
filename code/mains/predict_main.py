c8dcc9cd3a68bd10a3fec7aebcbd43dee6d045cd
melanibe <melanibe@student.ethz.ch>
2019-01-08 10:17:26 +0100

    adapted predict to choose the checkpoint manually

--- code/mains/predict_main.py
+++ code/mains/predict_main.py
@@ -11,6 +11,8 @@ from models.CBDP2_model import CBDP2Model
 from models.SimpleCNN_model import SimpleCNNModel
 from models.inception_model import InceptionModel
 from models.resNet_model import ResNetModel
+from models.kaggle_model import KaggleModel
+from models.DeepSimple_model import DeepSimpleModel
 from utils.config import process_config
 from utils.utils import get_args
 from utils.predictor import Predictor
@@ -30,7 +32,8 @@ def main():
 
     except Exception:
         print("missing or invalid arguments")
-        exit(0)
+        raise
+
 
     # create tensorflow session
     sess = tf.Session()
@@ -39,7 +42,7 @@ def main():
     # create an instance of the model you want
     try:
         if config.model == "DeepYeast":
-            model = DeepYeastModel(config)
+             model = DeepYeastModel(config)
         elif config.model == "SimpleCNN":
             model = SimpleCNNModel(config)
         elif config.model == "CP2":
@@ -58,15 +61,23 @@ def main():
             model = InceptionModel(config)
         elif config.model == "ResNet":
             model = ResNetModel(config)
+        elif config.model == "Kaggle":
+            model = KaggleModel(config)
+        elif config.model == "DeepSimple":
+            model = DeepSimpleModel(config)
     except AttributeError:
         print("The model to use is not specified in the config file")
         exit(1)
+    
     # load model if exists
-    model.load(sess)
+    model.load(sess, args.checkpoint_nb)
     # here you predict from your model
     predictor = Predictor(sess, model, config)
     predictor.predict(testIterator)
 
 
 if __name__ == '__main__':
+    # if you want to specify the model number manually
+    # because on other machine than training machine
+    # get latest checkpoint does not work
     main()
