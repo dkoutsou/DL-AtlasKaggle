50ce81622df696df9f88d99e5ff797b967ca897a
Andreas Georgiou <geandrea@student.ethz.ch>
2019-01-12 15:22:01 +0100

    Fixed main prediction bug

--- code/mains/predict_from_several_main.py
+++ code/mains/predict_from_several_main.py
@@ -1,18 +1,7 @@
 import tensorflow as tf
 
 from data_loader.data_generator import DataTestLoader
-from models.DeepYeast_model import DeepYeastModel
-from models.CP2_model import CP2Model
-from models.CP4_model import CP4Model
-from models.CDP4_model import CDP4Model
-from models.CBDP4_model import CBDP4Model
-from models.CDP2_model import CDP2Model
-from models.CBDP2_model import CBDP2Model
-from models.SimpleCNN_model import SimpleCNNModel
-from models.inception_model import InceptionModel
-from models.resNet_model import ResNetModel
-from models.kaggle_model import KaggleModel
-from models.DeepSimple_model import DeepSimpleModel
+from models.models import all_models
 from utils.config import process_config
 from utils.utils import get_args
 from utils.predictor import Predictor, get_pred_from_probas_threshold
@@ -49,30 +38,8 @@ def main():
         testIterator = DataTestLoader(config)
         # create an instance of the model you want
         try:
-            if config.model == "DeepYeast":
-                model = DeepYeastModel(config)
-            elif config.model == "SimpleCNN":
-                model = SimpleCNNModel(config)
-            elif config.model == "CP2":
-                model = CP2Model(config)
-            elif config.model == "CP4":
-                model = CP4Model(config)
-            elif config.model == "CDP4":
-                model = CDP4Model(config)
-            elif config.model == "CBDP4":
-                model = CBDP4Model(config)
-            elif config.model == "CDP2":
-                model = CDP2Model(config)
-            elif config.model == "CBDP2":
-                model = CBDP2Model(config)
-            elif config.model == "Inception":
-                model = InceptionModel(config)
-            elif config.model == "ResNet":
-                model = ResNetModel(config)
-            elif config.model == "Kaggle":
-                model = KaggleModel(config)
-            elif config.model == "DeepSimple":
-                model = DeepSimpleModel(config)
+            ModelInit = all_models[config.model]
+            model = ModelInit(config)
         except AttributeError:
             print("The model to use is not specified in the config file")
             exit(1)
@@ -84,14 +51,15 @@ def main():
         probas[i, :, :] = predictor.predict_probas(testIterator)
         print('processed {} model'.format(model))
         i += 1
-    print(np.shape(np.mean(probas, axis=0)))
+    probas = np.mean(probas, axis=0)
+    print(np.shape(probas))
     one_hot_pred = get_pred_from_probas_threshold(probas)
     bin = MultiLabelBinarizer(classes=np.arange(28))
     bin.fit([[1]])  # needed for instantiation of the object
     pred = bin.inverse_transform(one_hot_pred)
     predicted_labels = [
-        ' '.join([str(p) for p in sample_pred])
-        for sample_pred in pred]
+        ' '.join([str(p) for p in sample_pred]) for sample_pred in pred
+    ]
     print(np.shape(predicted_labels))
     testIterator.result['Predicted'] = predicted_labels
     testIterator.result = testIterator.result.sort_values(by='Id')
